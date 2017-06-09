a6c37cdbb3fe3664a7fec266660d7874ce3ddff1
Yegor Litsov <yegor.litsov@digitalhanger.com>
2017-06-09 16:37:15 +0300

    changed host link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  host: "https://api.themoviedb.org/3/genre/28/movies?api_key=f2c299854f7ac8e69ef49715399c1bbe&language=en-US&include_adult=false&sort_by=created_at.asc"
+  host: "https://api.themoviedb.org/3/movie/popular?api_key=f2c299854f7ac8e69ef49715399c1bbe&language=en-US&page=1"
 };
